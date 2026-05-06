1ff96706e52ec8fc4d779d5ab236505588b38bfd
Mario Cardozo <mariooscarcardozo0@gmail.com>
2026-05-06 20:31:16 -0300

    Servir landing directamente en inicio

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/sistema-cursos.html');
+    $path = public_path('sistema-cursos.html');
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    return response(file_get_contents($path), 200)
+        ->header('Content-Type', 'text/html');
 });
