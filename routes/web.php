2695d65e8755b6b596716b8b45de0a8e6adfb38d
Mario Cardozo <mariooscarcardozo0@gmail.com>
2026-05-06 20:08:35 -0300

    Cargar landing page desde la ruta principal

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/sistema-cursos.html');
+    return response()->file(public_path('sistema-cursos.html'));
 });
